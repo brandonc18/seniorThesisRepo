9e173d007673e62912767763a8357b7fed1a4925
Brandon Carls <bcarls@carthage.edu>
2025-10-12 22:17:07 -0500

    Initial start for testing bitboards

--- code/main.cpp
+++ code/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+
 using namespace std;
+using U64 = uint64_t;
+struct Board {
+	// Bitboards for each piece type
+	U64 white_pawns, white_knights, white_bishops, white_rooks, white_queens, white_king;
+    U64 black_pawns, black_knights, black_bishops, black_rooks, black_queens, black_king;
+	// Turn determinator, games start on white
+	bool white_turn = true;
+	// ...
+	// More likely needed at some point
+}
 
 int main()
 {
-    cout<<"Senior Thesis Project"<<endl;
+    cout<< "Chess Engine Project" <<endl;
 
     return 0;
 }
