9d972fb8ff2261f7b1a009a07dfc28c968c1a939
Brandon Carls <bcarls@carthage.edu>
2025-10-12 22:53:34 -0500

    Made position occupied and print board functions in board struct

--- code/main.cpp
+++ code/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 using namespace std;
+
 using U64 = uint64_t;
 struct Board {
 	// Bitboards for each piece type
@@ -12,6 +13,7 @@ struct Board {
 	// More likely needed at some point
 	//
 	
+	// Constructor with initial board setup
 	Board() {
 		white_pawns = 0x000000000000FF00ULL;	// Rank 2
 		white_knights = 0x0000000000000042ULL;	// B1 and G1
@@ -26,7 +28,37 @@ struct Board {
 		black_queens = 0x1000000000000000ULL;	// D8
 		black_king = 0x0800000000000000ULL;		// E8
 	}
-}
+
+	U64 occupied() {
+		return white_pawns | white_knights | white_bishops | white_rooks | white_queens | white_king | black_pawns | black_knights | black_bishops | black_rooks | black_queens | black_king;
+	}
+
+	void print() {
+		U64 occupied = this->occupied();
+		for (int row = 7; row >= 0; row--) {
+			cout << row + 1 << " ";
+			for (int col = 0; col < 8; col++) {
+				int positionNum = row * 8 + col;
+				U64 position = U64(1) << positionNum;
+				char p = '-';
+				if (position & white_pawns) p = 'P';
+				else if (position & white_knights) p = 'N';
+				else if (position & white_bishops) p = 'B';
+				else if (position & white_rooks) p = 'R';
+				else if (position & white_queens) p = 'Q';
+				else if (position & white_king) p = 'K';
+				else if (position & black_pawns) p = 'p';
+				else if (position & black_rooks) p = 'r';
+				else if (position & black_bishops) p = 'b';
+				else if (position & black_queens) p = 'q';
+				else if (position & black_king) p = 'k';
+				else if (position & black_knights) p = 'n';
+				cout << p << endl;
+			}
+		}
+		cout << "  a b c d e f g h\n";
+	}
+};
 
 int main()
 {
