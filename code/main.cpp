1ba0e6801f6f620d59d396ffffb5b423ed58ce27
Brandon Carls <bcarls@carthage.edu>
2025-11-14 13:11:30 -0600

    Print out pawn moves

--- code/main.cpp
+++ code/main.cpp
@@ -8,15 +8,15 @@ using namespace std;
 int main() {
   Board board;
   MoveList moves;
-  Move move(board.e2, board.e4, false, false, false, false);
-  Move mo(board.e2, board.e4);
+  // Move move(board.e2, board.e4, false, false, false, false);
+  // Move mo(board.e2, board.e4);
   MoveGen g;
   g.generateAllMoves(board, moves);
-  system("clear");
+  // system("clear");
   board.print();
-  board.makeMove(move);
-  system("clear");
-  board.print();
+  // board.makeMove(move);
+    // system("clear");
+    // board.print();
 
   for (int i = 0; i < moves.size(); i++) {
     cout << moves[i].from_square << " to " << moves[i].to_square << endl;
