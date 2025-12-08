f1dffbd1cc90ab1102a244b7d810f5d3c3a81afb
Brandon Carls <bcarls@carthage.edu>
2025-12-08 00:32:17 -0600

    Updated MoveGen

--- code/MoveGen.cpp
+++ code/MoveGen.cpp
@@ -1,199 +1,283 @@
 #include "MoveGen.h"
-#include <iostream>
-// #include "chess-types.h"
+#include "Board.h"
+#include "Magic.h"
 
 MoveGen::MoveGen() {
-  for (int sq = 0; sq < 64; sq++) {
-	precomputePawns(sq);
-	precomputeKnights(sq);
-	precomputeKings(sq);
-  }
+	// Precompute moves for each square of the board
+	for (int sq = 0; sq < 64; sq++) {
+		precomputePawns(sq);
+		precomputeKnights(sq);
+		precomputeKings(sq);
+		precomputeRooks(sq);
+		precomputeBishops(sq);
+	}
 }
 
 void MoveGen::precomputePawns(int sq) {
-  // Precompute Pawn attacks
-  // White attacks
-  if ((sq & 7) != 0)
-	PAWN_ATTACKS[sq][0].set_bit(sq + 7); // not a-file
-  if ((sq & 7) != 7)
-	PAWN_ATTACKS[sq][0].set_bit(sq + 9); // not h-file
+	// Precompute Pawn attacks
+	// White attacks
+	if ((sq & 7) != 0)
+		PAWN_ATTACKS[sq][0].set_bit(sq + 7); // not a-file
+	if ((sq & 7) != 7)
+		PAWN_ATTACKS[sq][0].set_bit(sq + 9); // not h-file
 
-  // Black attacks
-  if ((sq & 7) != 0)
-	PAWN_ATTACKS[sq][1].set_bit(sq - 9);
-  if ((sq & 7) != 7)
-	PAWN_ATTACKS[sq][1].set_bit(sq - 7);
+	// Black attacks
+	if ((sq & 7) != 0)
+		PAWN_ATTACKS[sq][1].set_bit(sq - 9);
+	if ((sq & 7) != 7)
+		PAWN_ATTACKS[sq][1].set_bit(sq - 7);
 }
 
 void MoveGen::precomputeKnights(int sq) {
-  // Precompute Knight movements/attacks
-  if (sq <= 45 && sq >= 18) {
-	KNIGHT_ATTACKS[sq].set_bit(sq - 17);
-	KNIGHT_ATTACKS[sq].set_bit(sq + 15);
-	KNIGHT_ATTACKS[sq].set_bit(sq - 15);
-	KNIGHT_ATTACKS[sq].set_bit(sq + 17);
-	KNIGHT_ATTACKS[sq].set_bit(sq + 6);
-	KNIGHT_ATTACKS[sq].set_bit(sq - 10);
-	KNIGHT_ATTACKS[sq].set_bit(sq + 10);
-	KNIGHT_ATTACKS[sq].set_bit(sq - 6);
-  } else {
-	if ((sq & 7) != 0) {
-	  if (sq > 16) 
+	// Precompute Knight movements/attacks
+	if (sq <= 45 && sq >= 18) {
 		KNIGHT_ATTACKS[sq].set_bit(sq - 17);
-	  if (sq < 48)
 		KNIGHT_ATTACKS[sq].set_bit(sq + 15);
-	  if ((sq & 7) != 1) {
-		if (sq < 56)
-		  KNIGHT_ATTACKS[sq].set_bit(sq + 6);
-		if (sq > 8)
-		  KNIGHT_ATTACKS[sq].set_bit(sq - 10);
-	  }
-	}
-	if ((sq & 7) != 7) {
-	  if (sq > 16) 
 		KNIGHT_ATTACKS[sq].set_bit(sq - 15);
-	  if (sq < 48)
 		KNIGHT_ATTACKS[sq].set_bit(sq + 17);
-	  if ((sq & 7) != 6) {
-		if (sq < 56)
-		  KNIGHT_ATTACKS[sq].set_bit(sq + 10);
-		if (sq > 8)
-		  KNIGHT_ATTACKS[sq].set_bit(sq - 6);
-	  }
+		KNIGHT_ATTACKS[sq].set_bit(sq + 6);
+		KNIGHT_ATTACKS[sq].set_bit(sq - 10);
+		KNIGHT_ATTACKS[sq].set_bit(sq + 10);
+		KNIGHT_ATTACKS[sq].set_bit(sq - 6);
+	} else {
+		if ((sq & 7) != 0) {
+			if (sq > 16)
+				KNIGHT_ATTACKS[sq].set_bit(sq - 17);
+			if (sq < 48)
+				KNIGHT_ATTACKS[sq].set_bit(sq + 15);
+			if ((sq & 7) != 1) {
+				if (sq < 56)
+					KNIGHT_ATTACKS[sq].set_bit(sq + 6);
+				if (sq > 8)
+					KNIGHT_ATTACKS[sq].set_bit(sq - 10);
+			}
+		}
+		if ((sq & 7) != 7) {
+			if (sq > 16)
+				KNIGHT_ATTACKS[sq].set_bit(sq - 15);
+			if (sq < 48)
+				KNIGHT_ATTACKS[sq].set_bit(sq + 17);
+			if ((sq & 7) != 6) {
+				if (sq < 56)
+					KNIGHT_ATTACKS[sq].set_bit(sq + 10);
+				if (sq > 8)
+					KNIGHT_ATTACKS[sq].set_bit(sq - 6);
+			}
+		}
 	}
-  }
 }
 
 void MoveGen::precomputeKings(int sq) {
-  // Precompute King movements/attacks
-  if (sq > 7) 
-	KING_MOVES[sq].set_bit(sq - 8);	// King down
-  if ((sq & 7) != 0)
-	KING_MOVES[sq].set_bit(sq - 1); // King left
-  if ((sq & 7) != 7)
-	KING_MOVES[sq].set_bit(sq + 1); // King right
-  if (sq < 56)
-	KING_MOVES[sq].set_bit(sq + 8); // King up
-  if (sq > 7 && ((sq & 7) != 0))
-	KING_MOVES[sq].set_bit(sq - 9); // King down left
-  if (sq > 7 && ((sq & 7) != 7))
-	KING_MOVES[sq].set_bit(sq - 7); // King down right
-  if (sq < 56 && ((sq & 7) != 0))
-	KING_MOVES[sq].set_bit(sq + 7); // King up left
-  if (sq < 56 && ((sq & 7) != 7))
-	KING_MOVES[sq].set_bit(sq + 9); // King up right
+	// Precompute King movements/attacks
+	if (sq > 7)
+		KING_MOVES[sq].set_bit(sq - 8); // King down
+	if ((sq & 7) != 0)
+		KING_MOVES[sq].set_bit(sq - 1); // King left
+	if ((sq & 7) != 7)
+		KING_MOVES[sq].set_bit(sq + 1); // King right
+	if (sq < 56)
+		KING_MOVES[sq].set_bit(sq + 8); // King up
+	if (sq > 7 && ((sq & 7) != 0))
+		KING_MOVES[sq].set_bit(sq - 9); // King down left
+	if (sq > 7 && ((sq & 7) != 7))
+		KING_MOVES[sq].set_bit(sq - 7); // King down right
+	if (sq < 56 && ((sq & 7) != 0))
+		KING_MOVES[sq].set_bit(sq + 7); // King up left
+	if (sq < 56 && ((sq & 7) != 7))
+		KING_MOVES[sq].set_bit(sq + 9); // King up right
+}
+
+void MoveGen::precomputeRooks(int sq) {
+	// Precompute Rook movements/attacks using magic bitboards
+	U64 mask = rmask(sq);
+	int bits = RBits[sq];
+	int combinations = 1 << bits;
+
+	for (int i = 0; i < combinations; i++) {
+		U64 occupancy = index_to_uint64(i, bits, mask);
+		U64 attacks = ratt(sq, occupancy);
+		int index = transform(occupancy, RMagic[sq], bits);
+		ROOK_ATTACKS[sq][index].set_raw(attacks);
+	}
+}
+
+void MoveGen::precomputeBishops(int sq) {
+	// Precompute Bishop movements/attacks using magic bitboards
+	U64 mask = bmask(sq);
+	int bits = BBits[sq];
+	int combinations = 1 << bits;
+
+	for (int i = 0; i < combinations; i++) {
+		U64 occupancy = index_to_uint64(i, bits, mask);
+		U64 attacks = batt(sq, occupancy);
+		int index = transform(occupancy, BMagic[sq], bits);
+		BISHOP_ATTACKS[sq][index].set_raw(attacks);
+	}
 }
 
 void MoveGen::generateAllMoves(Board &board, MoveList &moves) {
-  moves.clear();
+	moves.clear();
 
-  // Loop over all pawns of side to move
-  bool white = (board.getSideToMove() == ecWhite);
+	// Loop over all pawns of side to move
+	bool white = (board.getSideToMove() == ecWhite);
 
-  generatePawnMoves(board, moves, white);
-  generateKnightMoves(board, moves, white);
-  generateKingMoves(board, moves, white);
-
+	// Generate moves for each piece type
+	generatePawnMoves(board, moves, white);
+	generateKnightMoves(board, moves, white);
+	generateBishopMoves(board, moves, white);
+	generateRookMoves(board, moves, white);
+	generateQueenMoves(board, moves, white);
+	generateKingMoves(board, moves, white);
 }
 
 void MoveGen::generatePawnMoves(Board &board, MoveList &moves, bool white) {
-  Bitboard pawns = white ? board.getWhitePawns() : board.getBlackPawns();
-  int from_sq;
-  while ((from_sq = pawns.pop_lsb()) != -1) {
-    Bitboard targets = getPawnMoves(from_sq, white);
+	Bitboard pawns = white ? board.getWhitePawns() : board.getBlackPawns();
 
-    int to_sq;
-    while ((to_sq = targets.pop_lsb()) != -1) {
-      // Check if target is empty (quiet) or opponent (capture)
-	  bool is_capture = board.getOccupancy(!white).get_bit(to_sq);
-      bool promotion = white ? (to_sq / 8 == 7) : (to_sq / 8 == 0);
-	  if (!is_capture && (abs(from_sq - to_sq) % 8 != 0)) { // Only add capturing pawn move if it is actually capturing
-		continue;
-	  }
-      moves.push_back({from_sq, to_sq, promotion, is_capture, false, false});
-    }
-  }
+	int from_sq;
+	while ((from_sq = pawns.pop_lsb()) != -1) {
+		Bitboard targets = getPawnMoves(board, from_sq, white);
+
+		int to_sq;
+		while ((to_sq = targets.pop_lsb()) != -1) {
+			// TODO: en passent not yet implemented
+			bool capture = board.getOccupancy(!white).get_bit(to_sq);
+
+			// TODO: promotions not yet implemented
+			moves.push_back(Move(from_sq, to_sq, false, capture, false, to_sq == board.getEnPassantSquare()));
+		}
+	}
 }
 
 void MoveGen::generateKnightMoves(Board &board, MoveList &moves, bool white) {
-  Bitboard knights = white ? board.getWhiteKnights() : board.getBlackKnights();
-  int from_sq;
-  while ((from_sq = knights.pop_lsb()) != -1) {
-    Bitboard targets = KNIGHT_ATTACKS[from_sq];
-	int to_sq;
+	Bitboard knights = white ? board.getWhiteKnights() : board.getBlackKnights();
+	int from_sq;
+	while ((from_sq = knights.pop_lsb()) != -1) {
+		Bitboard targets = KNIGHT_ATTACKS[from_sq];
+		int to_sq;
 
-    while ((to_sq = targets.pop_lsb()) != -1) {
-      // Check if target is empty (quiet) or opponent (capture)
-	  bool is_capture = board.getOccupancy(!white).get_bit(to_sq);
-	  if (board.getOccupancy(white ? 0 : 1).get_bit(to_sq)) { // Can't capture own piece
-		continue;
-	  }
-      moves.push_back({from_sq, to_sq, false, is_capture, false, false});
-    }
-  }
+		while ((to_sq = targets.pop_lsb()) != -1) {
+			// Check if target is quiet or capture
+			bool is_capture = board.getOccupancy(!white).get_bit(to_sq);
+			if (board.getOccupancy(white ? 0 : 1).get_bit(to_sq)) { // Can't capture own piece
+				continue;
+			}
+			moves.push_back({from_sq, to_sq, false, is_capture, false, false});
+		}
+	}
 }
 
 void MoveGen::generateKingMoves(Board &board, MoveList &moves, bool white) {
-  Bitboard king = white ? board.getWhiteKing() : board.getBlackKing();
-  int from_sq;
-  while ((from_sq = king.pop_lsb()) != -1) {
-    Bitboard targets = KING_MOVES[from_sq];
-	int to_sq;
+	Bitboard king = white ? board.getWhiteKing() : board.getBlackKing();
+	int from_sq;
+	while ((from_sq = king.pop_lsb()) != -1) {
+		Bitboard targets = KING_MOVES[from_sq];
+		int to_sq;
 
-    while ((to_sq = targets.pop_lsb()) != -1) {
-      // Check if target is empty (quiet) or opponent (capture)
-	  bool is_capture = board.getOccupancy(!white).get_bit(to_sq);
-	  if (board.getOccupancy(white ? 0 : 1).get_bit(to_sq)) { // Can't capture own piece
-		continue;
-	  }
-      moves.push_back({from_sq, to_sq, false, is_capture, false, false});
-    }
-  }
+		while ((to_sq = targets.pop_lsb()) != -1) {
+			// Check if target is quiet or capture
+			bool is_capture = board.getOccupancy(!white).get_bit(to_sq);
+			if (board.getOccupancy(white ? 0 : 1).get_bit(to_sq)) { // Can't capture own piece
+				continue;
+			}
+			moves.push_back({from_sq, to_sq, false, is_capture, false, false});
+		}
+	}
 }
 
-// pseudo-legal pawn moves from one square
-Bitboard MoveGen::getPawnMoves(const int from_square, bool white) {
-  // If not valid square return 0 possible moves
-  if (from_square < 0 || from_square > 63) {
-    return Bitboard(0);
-  }
+Bitboard MoveGen::getPawnMoves(Board &board, int from_sq, bool white) {
+	Bitboard targets;
+	Bitboard occupied = board.getOccupied();
+	Bitboard opponents = board.getOccupancy(white ? BLACK_OCCUPIED : WHITE_OCCUPIED);
+	Bitboard empty = ~occupied;
 
-  int rank = from_square / 8; // Determine rank
+	int direction = white ? 8 : -8;
+	int to_single = from_sq + direction;
 
-  // Pawn shouldn't be able to be on promotion rank, but if so return 0 possible moves
-  if ((white && rank == 7) || (!white && rank == 0)) {
-    return Bitboard(0);
-  }
+	// Single push
+	if (to_single >= 0 && to_single < 64 && empty.get_bit(to_single)) {
+		targets.set_bit(to_single);
 
-  Bitboard moves = getPawnQuiets(from_square, white);
-  moves = moves | getPawnCaptures(from_square, white); // Combine bitboards of attacks and silent pushes
+		// Double push from starting rank
+		if ((white && from_sq < 16) || (!white && from_sq >= 48)) {
+			int to_double = to_single + direction;
+			if (empty.get_bit(to_double)) {
+				targets.set_bit(to_double);
+			}
+		}
+	}
 
-  return moves; // Bitboard with all target squares set
+	// Captures (only to opponent pieces)
+	Bitboard attacks = PAWN_ATTACKS[from_sq][white ? 0 : 1];
+	targets |= (attacks & opponents);
+
+	// En passant
+	int ep = board.getEnPassantSquare();
+	if (ep != -1 && attacks.get_bit(ep)) {
+		targets.set_bit(ep);
+	}
+
+	return targets;
 }
 
-// Helper to get forward moves
-Bitboard MoveGen::getPawnQuiets(const int from_square, bool white) {
-  Bitboard quiets;
-  if (white) {
-    if (from_square <= 55)
-      quiets.set_bit(from_square + 8);
-    if (from_square < 16)
-      quiets.set_bit(from_square + 16);
-  } else {
-    if (from_square >= 16)
-      quiets.set_bit(from_square - 8);
-    if (from_square > 55)
-      quiets.set_bit(from_square - 16);
-  }
+void MoveGen::generateQueenMoves(Board &board, MoveList &moves, bool white) {
+	Bitboard pieces = white ? board.getWhiteQueens() : board.getBlackQueens();
+	Bitboard own = board.getOccupancy(white ? WHITE_OCCUPIED : BLACK_OCCUPIED);
 
-  return quiets;
+	int sq;
+	while ((sq = pieces.pop_lsb()) != -1) {
+		// Use both the rook and bishop magic bitboards to get queen moves
+		// for the straight and vertical movements
+		Bitboard attacks = getRookAttacks(sq, board.getOccupied().raw()) | getBishopAttacks(sq, board.getOccupied().raw());
+		attacks &= ~own;
+
+		int to;
+		while ((to = attacks.pop_lsb()) != -1) {
+			bool capture = board.getOccupancy(!white).get_bit(to);
+			moves.push_back({sq, to, false, capture, false, false});
+		}
+	}
 }
 
-// Helper to get attacks
-Bitboard MoveGen::getPawnCaptures(const int from_square, bool white) {
-  return PAWN_ATTACKS[from_square][white ? 0 : 1];
+void MoveGen::generateRookMoves(Board &board, MoveList &moves, bool white) {
+	Bitboard pieces = white ? board.getWhiteRooks() : board.getBlackRooks();
+	Bitboard own = board.getOccupancy(white ? WHITE_OCCUPIED : BLACK_OCCUPIED);
+	int sq;
+	while ((sq = pieces.pop_lsb()) != -1) {
+		Bitboard attacks = getRookAttacks(sq, board.getOccupied().raw());
+		attacks &= ~own; // remove own pieces
+		int to;
+		while ((to = attacks.pop_lsb()) != -1) {
+			bool capture = board.getOccupancy(!white).get_bit(to);
+			moves.push_back({sq, to, false, capture, false, false});
+		}
+	}
 }
 
-Bitboard MoveGen::getKnightMoves(const int from_square, bool white) {
-  return Bitboard(0); 
+void MoveGen::generateBishopMoves(Board &board, MoveList &moves, bool white) {
+	Bitboard pieces = white ? board.getWhiteBishops() : board.getBlackBishops();
+	Bitboard own = board.getOccupancy(white ? WHITE_OCCUPIED : BLACK_OCCUPIED);
+
+	int sq;
+	while ((sq = pieces.pop_lsb()) != -1) {
+		Bitboard attacks = getBishopAttacks(sq, board.getOccupied().raw());
+		attacks &= ~own; // remove own pieces (legal moves only)
+
+		int to;
+		while ((to = attacks.pop_lsb()) != -1) {
+			bool capture = board.getOccupancy(!white).get_bit(to);
+			moves.push_back({sq, to, false, capture, false, false});
+		}
+	}
+}
+
+Bitboard MoveGen::getRookAttacks(int sq, U64 occupancy) {
+	occupancy &= rmask(sq);
+	int index = transform(occupancy, RMagic[sq], RBits[sq]);
+	return ROOK_ATTACKS[sq][index];
+}
+
+Bitboard MoveGen::getBishopAttacks(int sq, U64 occupancy) {
+	occupancy &= bmask(sq);
+	int index = transform(occupancy, BMagic[sq], BBits[sq]);
+	return BISHOP_ATTACKS[sq][index];
 }
--- code/MoveGen.h
+++ code/MoveGen.h
@@ -5,23 +5,35 @@
 using MoveList = vector<Move>;
 
 class MoveGen {
-public:
-  MoveGen();
-  void generateAllMoves(Board &board, MoveList &moves);
-  // ... knight, sliding, etc.
-private:
-  void generatePawnMoves(Board &board, MoveList &moves, bool white);
-  void generateKnightMoves(Board &board, MoveList &moves, bool white);
-  void generateKingMoves(Board &board, MoveList &moves, bool white);
-  void precomputePawns(int sq);
-  void precomputeKnights(int sq);
-  void precomputeKings(int sq);
-  Bitboard getPawnQuiets(const int from_square, bool white);
-  Bitboard getPawnCaptures(const int from_square, bool white);
-  Bitboard getPawnMoves(const int from_square, bool white);
-  Bitboard getKnightMoves(const int from_square, bool white);
+  public:
+	MoveGen();
+	void generateAllMoves(Board &board, MoveList &moves);
 
-  Bitboard PAWN_ATTACKS[64][2];
-  Bitboard KNIGHT_ATTACKS[64]; 
-  Bitboard KING_MOVES[64];
+  private:
+	// Generate moves helpers for each piece to be used by generateAllMoves
+	void generatePawnMoves(Board &board, MoveList &moves, bool white);
+	void generateKnightMoves(Board &board, MoveList &moves, bool white);
+	void generateKingMoves(Board &board, MoveList &moves, bool white);
+	void generateRookMoves(Board &board, MoveList &moves, bool white);
+	void generateBishopMoves(Board &board, MoveList &moves, bool white);
+	void generateQueenMoves(Board &board, MoveList &moves, bool white);
+
+	// Precompute moves for quicker move calculation
+	void precomputePawns(int sq);
+	void precomputeKnights(int sq);
+	void precomputeKings(int sq);
+	void precomputeRooks(int sq);
+	void precomputeBishops(int sq);
+
+	// Movement helpers for the generate functions
+	Bitboard getPawnMoves(Board &board, const int from_square, bool white);
+	Bitboard getRookAttacks(const int sq, U64 occupancy);
+	Bitboard getBishopAttacks(const int sq, U64 occupancy);
+	Bitboard getQueenAttacks(const int sq, U64 occupancy) { return getRookAttacks(sq, occupancy) | getBishopAttacks(sq, occupancy); }
+
+	Bitboard PAWN_ATTACKS[64][2];
+	Bitboard KNIGHT_ATTACKS[64];
+	Bitboard KING_MOVES[64];
+	Bitboard ROOK_ATTACKS[64][4096];
+	Bitboard BISHOP_ATTACKS[64][512];
 };
