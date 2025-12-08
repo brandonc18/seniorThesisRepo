569568a10e1af9b6bab881d225ce0571c8d68608
Brandon Carls <bcarls@carthage.edu>
2025-12-08 00:31:50 -0600

    Updated Bitboard.h

--- code/Bitboard.h
+++ code/Bitboard.h
@@ -2,60 +2,52 @@
 using U64 = uint64_t;
 
 class Bitboard {
-public:
+  public:
 	// Construct Bitboard at 0 or a given value
 	Bitboard(U64 init = 0ULL) : data(init) {}
-	
+
 	// Set a bit at square index
-	void set_bit(int square) {
-		data |= (1ULL << square);
-	}
+	void set_bit(int square) { data |= (1ULL << square); }
 
 	// Clear a bit at square index
-	void clear_bit(int square) {
-		data &= ~(1ULL << square);
-	}
+	void clear_bit(int square) { data &= ~(1ULL << square); }
 
 	// Get bit value at square index
-	bool get_bit(int square) {
-		return (data & (1ULL << square)) != 0;
-	}
+	bool get_bit(int square) { return (data & (1ULL << square)) != 0; }
 
 	// Pop the LSB and return its index
 	int pop_lsb() {
-		if (data == 0) return -1;
+		if (data == 0)
+			return -1;
 		int index = __builtin_ctzll(data);
-		data &= data -1;
+		data &= data - 1;
 		return index;
 	}
 
-	// Doesn't edit, just reads
-	int pop_lsb() const {
-        if (data == 0) return -1;
-        return __builtin_ctzll(data);
-    }
-
 	// Count set bits
-	int count() {
-		return __builtin_popcountll(data);
-	}
+	int count() { return __builtin_popcountll(data); }
 
 	// Overload bitwise operators for combining bitboards
-    Bitboard operator|(const Bitboard& other) const {
-        return Bitboard(data | other.data);
-    }
+	Bitboard operator|(const Bitboard &other) const { return Bitboard(data | other.data); }
 
-    Bitboard operator&(const Bitboard& other) const {
-        return Bitboard(data & other.data);
-    }
+	Bitboard operator&(const Bitboard &other) const { return Bitboard(data & other.data); }
 
-    Bitboard operator~() const {
-        return Bitboard(~data);
-    }
+	Bitboard operator~() const { return Bitboard(~data); }
 
-    // Get the underlying data
-    U64 raw() const { return data; }
-    void set_raw(U64 val) { data = val; }
-private:
+	Bitboard &operator&=(const Bitboard &other) {
+		data &= other.data;
+		return *this;
+	}
+
+	Bitboard &operator|=(const Bitboard &other) {
+		data |= other.data;
+		return *this;
+	}
+
+	// Get the underlying data
+	U64 raw() const { return data; }
+	void set_raw(U64 val) { data = val; }
+
+  private:
 	U64 data;
 };
